4f1226bd96befc5f87aeeaa4afba690048059c1e
alimagdi12 <alimagdi12367@yahoo.com>
2024-05-07 22:41:05 +0300

    first commit

---
+++ Unit_Testing_2/src/posts/posts.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../App.css";
+import Post1 from "./post1";
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        setPosts(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        setIsError(true);
+        setIsLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="posts">
+          {isLoading && <div className="loader"></div>}
+          {isError && <div className="error">Something went wrong!</div>}
+          {!isLoading && !isError && posts.length === 0 && (
+            <div className="no-posts">There are no posts.</div>
+          )}
+          {posts.map((post) => (
+            <Post1 key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
